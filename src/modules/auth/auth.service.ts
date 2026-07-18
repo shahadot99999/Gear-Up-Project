75bc8577e6d9e198e51621ca0a05573000377e51
shahadot99999 <shahadot99999@gmail.com>
2026-07-18 06:14:03 +0600

    refresh token are created

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface"
 import { jwtUtils } from "../../utils/jwt";
 import config from "../../config";
-import { SignOptions } from "jsonwebtoken";
+import { JwtPayload, SignOptions } from "jsonwebtoken";
 
 
 const loginUser = async(payload: ILoginUser)=>{
@@ -47,6 +47,43 @@ const loginUser = async(payload: ILoginUser)=>{
     };
 }
 
+const refreshToken = async (refreshToken : string) => {
+    const verifiedRefreshToken = jwtUtils.verifyToken(refreshToken, config.jwt_refresh_secret);
+
+    if(!verifiedRefreshToken.success){
+        throw new Error(verifiedRefreshToken.error)
+    }
+
+    const {id} = verifiedRefreshToken.data as JwtPayload;
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where : {
+            id
+        }
+    })
+
+    // if(user.activeStatus === "BLOCKED"){
+    //     throw new Error("User is blocked!")
+    // }
+
+    const jwtPayload = {
+        id,
+        name : user.name,
+        email : user.email,
+        role : user.role
+    }
+
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_access_secret,
+        config.jwt_access_expires_in as SignOptions
+    );
+
+    return {accessToken}
+}
+
 export const authService = {
-    loginUser
+    loginUser,
+    refreshToken
 }
