8026d6a12335ad400b964fac992ce932a7097e93
shahadot99999 <shahadot99999@gmail.com>
2026-07-27 18:20:34 +0600

    auth route login problem solve.

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -3,8 +3,10 @@ import { authController } from "./auth.controller";
 
 const router = Router();
 
+//login route set up
 router.post("/login", authController.loginUser);
 
+//refresh token route set up
 router.post("/refresh-token", authController.refreshToken);
 
 export const authRoutes = router;
